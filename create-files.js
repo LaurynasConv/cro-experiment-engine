9f72f5efbff2cf05d5699c6ad2170535ab713f71
Laurynas Grigutis <laurynas@conversiondev.com>
2021-05-06 19:37:08 +0300

    Create client code in __dev for each experience

--- create-files.js
+++ create-files.js
@@ -11,6 +11,48 @@ const fs = require('fs-extra');
 
 const { getPaths } = require('./paths');
 
+const getClientCode = () => {
+  const { expPath } = getPaths();
+  return `/* eslint-disable */
+function trigger() {
+  var socketScript = document.createElement('script');
+  var style = document.createElement('style');
+
+  socketScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/3.1.1/socket.io.js';
+  socketScript.addEventListener('load', function() {
+    var socket = io('http://localhost:4000', {
+      query: { id: '${expPath}' },
+    });
+
+    socket.on('js', function(js) {
+      console.log({ js: js });
+
+      try {
+        eval('(function() {'+js+'})()');
+      } catch (err) {
+        console.error('Error occured in the Experience script');
+        console.error(err);
+      }
+    });
+
+    socket.on('css', function(css) {
+      console.log({ css: css });
+
+      style.innerHTML = css;
+    });
+  });
+
+  document.head.appendChild(style);
+  document.head.appendChild(socketScript);
+  window.__con_dev = window.__con_dev || {};
+  window.__con_dev['${expPath}'] = { injected: true };
+};
+
+if (!(window.__con_dev && window.__con_dev['${expPath}'])) {
+  trigger();
+}`;
+};
+
 exports.createFiles = () =>
   new Promise(resolve => {
     const { sourceDir, expDir, devDir } = getPaths();
@@ -27,6 +69,8 @@ exports.createFiles = () =>
         fs.ensureFileSync(entrySassPath);
         fs.ensureFileSync(path.join(expDir, 'activation.js'));
         fs.ensureFileSync(path.join(expDir, 'shared.js'));
+        fs.ensureFileSync(path.join(devDir, 'cleanup.js'));
+        fs.writeFileSync(path.join(devDir, 'client.js'), getClientCode());
 
         setTimeout(resolve, 50);
       });
--- paths.js
+++ paths.js
@@ -1,15 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires, no-console */
 const path = require('path');
 
-let paths = { rootDir: '', expDir: '', sourceDir: '', devDir: '' };
+let paths = { rootDir: '', expDir: '', sourceDir: '', devDir: '', expPath: '' };
 
 const setUpPaths = providedExp => {
   const rootDir = path.join(__dirname, '../experiments');
   const expDir = providedExp ? path.join(process.cwd(), providedExp) : process.cwd();
   const sourceDir = path.join(expDir, 'source');
   const devDir = path.join(expDir, '__dev');
+  const expPath = expDir.replace(new RegExp(`${rootDir}/?`), '');
 
-  paths = { rootDir, expDir, sourceDir, devDir };
+  paths = { rootDir, expDir, sourceDir, devDir, expPath };
   console.log(paths);
   return paths;
 };
