8a069a352e2db76be63755ea3539f1c2e954d96c
Laurynas Grigutis <laurynas@conversiondev.com>
2021-06-03 06:30:20 +0300

    - Updated client.js - Removed activation.js & shared.js

--- create-files.js
+++ create-files.js
@@ -15,42 +15,55 @@ const getClientCode = () => {
   const { expPath } = getPaths();
   return `/* eslint-disable */
 function trigger() {
+  var experiment = '${expPath}';
   var socketScript = document.createElement('script');
   var style = document.createElement('style');
+  var injectJs = function(js) {
+    try {
+      eval('(function() {'+js+'})()');
+    } catch (err) {
+      console.error('Error occured in the Experience script');
+      console.error(err);
+    }
+  }
 
-  socketScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/3.1.1/socket.io.js';
-  socketScript.addEventListener('load', function() {
-    var socket = io('http://localhost:4000', {
-      query: { id: '${expPath}' },
-    });
-
-    socket.on('js', function(js) {
-      console.log({ js: js });
-
-      try {
-        eval('(function() {'+js+'})()');
-      } catch (err) {
-        console.error('Error occured in the Experience script');
-        console.error(err);
-      }
-    });
-
-    socket.on('css', function(css) {
-      console.log({ css: css });
-
-      style.innerHTML = css;
-    });
-  });
-
-  document.head.appendChild(style);
-  document.head.appendChild(socketScript);
   window.__con_dev = window.__con_dev || {};
-  window.__con_dev['${expPath}'] = { injected: true };
+  window.__con_dev[experiment] = window.__con_dev[experiment] || { injected: true };
+
+  if (window.__con_dev[experiment].code) {
+    injectJs(window.__con_dev[experiment].code);
+  } else {
+    socketScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/3.1.1/socket.io.js';
+    socketScript.addEventListener('load', function() {
+      var socket = io('http://localhost:4000', {
+        query: { id: experiment },
+      });
+
+      socket.on('js', function(js) {
+        console.log({ js: js });
+
+        try {
+          window.__con_dev[experiment].code = js;
+          injectJs(js);
+        } catch (err) {
+          console.error('Error occured in the Experience script');
+          console.error(err);
+        }
+      });
+
+      socket.on('css', function(css) {
+        console.log({ css: css });
+
+        style.innerHTML = css;
+      });
+    });
+
+    document.head.appendChild(style);
+    document.head.appendChild(socketScript);
+  }
 };
 
-if (!(window.__con_dev && window.__con_dev['${expPath}'])) {
-  trigger();
-}`;
+trigger();`;
 };
 
 exports.createFiles = () =>
@@ -58,6 +71,8 @@ exports.createFiles = () =>
     const { sourceDir, expDir, devDir } = getPaths();
     const entryTsPath = path.join(sourceDir, 'index.ts');
     const entrySassPath = path.join(sourceDir, 'index.scss');
+    const clientJsPath = path.join(devDir, 'client.js');
+    const cleanupPath = path.join(devDir, 'cleanup.js');
 
     if (!fs.existsSync(entryTsPath)) {
       rl.question(`No experience found in ${expDir}\nPress Enter to create it or "CTRL+C" to exit: `, () => {
@@ -67,14 +82,16 @@ exports.createFiles = () =>
 
         fs.ensureFileSync(entryTsPath);
         fs.ensureFileSync(entrySassPath);
-        fs.ensureFileSync(path.join(expDir, 'activation.js'));
-        fs.ensureFileSync(path.join(expDir, 'shared.js'));
-        fs.ensureFileSync(path.join(devDir, 'cleanup.js'));
-        fs.writeFileSync(path.join(devDir, 'client.js'), getClientCode());
+        fs.ensureFileSync(cleanupPath);
+        fs.writeFileSync(clientJsPath, getClientCode());
 
         setTimeout(resolve, 50);
       });
     } else {
+      if (!fs.existsSync(clientJsPath)) {
+        fs.writeFileSync(clientJsPath, getClientCode());
+      }
+      fs.ensureFileSync(cleanupPath);
       resolve();
     }
   });
