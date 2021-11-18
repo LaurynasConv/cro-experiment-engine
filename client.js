05ce111e80717b6cb9c73afcc5b574567f3f1aae
Laurynas Grigutis <laurynas@conversiondev.com>
2021-11-19 01:10:47 +0200

    Use client.js as template

--- client.js
+++ client.js
@@ -1,37 +1,51 @@
 /* eslint-disable */
 function trigger() {
+  var experiment = '`{{expPath}}`';
   var socketScript = document.createElement('script');
   var style = document.createElement('style');
+  var injectJs = function(js) {
+    try {
+      eval('(function() {'+js+'})()');
+    } catch (err) {
+      console.error('Error occured in the Experience script');
+      console.error(err);
+    }
+  }
 
-  socketScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/3.1.1/socket.io.js';
-  socketScript.addEventListener('load', function() {
-    var socket = io('http://localhost:4000', {
-      query: { id: 'path/to/experiment' }, // Change this to the experiment path
+  window.__con_dev = window.__con_dev || {};
+  window.__con_dev[experiment] = window.__con_dev[experiment] || { injected: true };
+
+  if (window.__con_dev[experiment].code) {
+    injectJs(window.__con_dev[experiment].code);
+  } else {
+    socketScript.src = 'http://localhost:4000/libs/socket.io/3.1.1/socket.io.js';
+    socketScript.addEventListener('load', function() {
+      var socket = __con_dev.io('http://localhost:4000', {
+        query: { id: experiment },
+      });
+
+      socket.on('js', function(js) {
+        console.log({ js: js });
+
+        try {
+          window.__con_dev[experiment].code = js;
+          injectJs(js);
+        } catch (err) {
+          console.error('Error occured in the Experience script');
+          console.error(err);
+        }
+      });
+
+      socket.on('css', function(css) {
+        console.log({ css: css });
+
+        style.innerHTML = css;
+      });
     });
 
-    socket.on('js', function(js) {
-      console.log({ js: js });
-
-      try {
-        eval('(function() {'+js+'})()');
-      } catch (err) {
-        console.error('Error occured in the Experience script');
-        console.error(err);
-      }
-    });
-
-    socket.on('css', function(css) {
-      console.log({ css: css });
-
-      style.innerHTML = css;
-    });
-  });
-
-  document.head.appendChild(style);
-  document.head.appendChild(socketScript);
-  window.__con_dev = { injected: true };
+    document.head.appendChild(style);
+    document.head.appendChild(socketScript);
+  }
 };
 
-if (!window.__con_dev) {
-  trigger();
-}
+trigger();
--- create-files.js
+++ create-files.js
@@ -12,57 +12,8 @@ const fs = require('fs-extra');
 const { getPaths } = require('./paths');
 
 /** @argument expPath {string} */
-const getClientCode = expPath => `/* eslint-disable */
-function trigger() {
-  var experiment = '${expPath}';
-  var socketScript = document.createElement('script');
-  var style = document.createElement('style');
-  var injectJs = function(js) {
-    try {
-      eval('(function() {'+js+'})()');
-    } catch (err) {
-      console.error('Error occured in the Experience script');
-      console.error(err);
-    }
-  }
-
-  window.__con_dev = window.__con_dev || {};
-  window.__con_dev[experiment] = window.__con_dev[experiment] || { injected: true };
-
-  if (window.__con_dev[experiment].code) {
-    injectJs(window.__con_dev[experiment].code);
-  } else {
-    socketScript.src = 'http://localhost:4000/libs/socket.io/3.1.1/socket.io.js';
-    socketScript.addEventListener('load', function() {
-      var socket = __con_dev.io('http://localhost:4000', {
-        query: { id: experiment },
-      });
-
-      socket.on('js', function(js) {
-        console.log({ js: js });
-
-        try {
-          window.__con_dev[experiment].code = js;
-          injectJs(js);
-        } catch (err) {
-          console.error('Error occured in the Experience script');
-          console.error(err);
-        }
-      });
-
-      socket.on('css', function(css) {
-        console.log({ css: css });
-
-        style.innerHTML = css;
-      });
-    });
-
-    document.head.appendChild(style);
-    document.head.appendChild(socketScript);
-  }
-};
-
-trigger();`;
+const clientCode = fs.readFileSync(path.join(__dirname, 'client.js'), 'utf-8');
+const getClientCode = expPath => clientCode.replace(/`{{expPath}}`/g, expPath);
 
 exports.createFiles = async () => {
   const { variations } = getPaths();
